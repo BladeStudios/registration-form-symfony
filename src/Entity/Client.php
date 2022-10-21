2fe873f85a4d07ceb5e03ffb39734973b880f20d
Blade Studios <maciejwarakomski@gmail.com>
2022-10-21 10:53:04 +0200

    created Client entity and repository

---
+++ src/Entity/Client.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClientRepository::class)
+ * @ORM\Table(name="clients")
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street_name;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $street_number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $postal_code;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $voivodeship;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $pesel;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $nip;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_individual_client;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStreetName(): ?string
+    {
+        return $this->street_name;
+    }
+
+    public function setStreetName(string $street_name): self
+    {
+        $this->street_name = $street_name;
+
+        return $this;
+    }
+
+    public function getStreetNumber(): ?string
+    {
+        return $this->street_number;
+    }
+
+    public function setStreetNumber(string $street_number): self
+    {
+        $this->street_number = $street_number;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(string $postal_code): self
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getVoivodeship(): ?string
+    {
+        return $this->voivodeship;
+    }
+
+    public function setVoivodeship(string $voivodeship): self
+    {
+        $this->voivodeship = $voivodeship;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPesel(): ?string
+    {
+        return $this->pesel;
+    }
+
+    public function setPesel(?string $pesel): self
+    {
+        $this->pesel = $pesel;
+
+        return $this;
+    }
+
+    public function getNip(): ?string
+    {
+        return $this->nip;
+    }
+
+    public function setNip(?string $nip): self
+    {
+        $this->nip = $nip;
+
+        return $this;
+    }
+
+    public function isIsIndividualClient(): ?bool
+    {
+        return $this->is_individual_client;
+    }
+
+    public function setIsIndividualClient(bool $is_individual_client): self
+    {
+        $this->is_individual_client = $is_individual_client;
+
+        return $this;
+    }
+}
