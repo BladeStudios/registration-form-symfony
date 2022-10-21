6ffff36c0ff36d0224d898b68df3fe69a6f05b49
Blade Studios <maciejwarakomski@gmail.com>
2022-10-21 11:01:56 +0200

    added field labels, types and options for ClientFormType in buildForm() method

--- src/Form/ClientFormType.php
+++ src/Form/ClientFormType.php
@@ -6,23 +6,55 @@ use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ClientFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('street_name')
-            ->add('street_number')
-            ->add('city')
-            ->add('postal_code')
-            ->add('voivodeship')
-            ->add('phone')
-            ->add('email')
-            ->add('pesel')
-            ->add('nip')
-            ->add('is_individual_client')
+            ->add('is_individual_client', ChoiceType::class, [
+                'label' => 'Status prawny:',
+                'expanded' => true,
+                'multiple' => false,
+                'choices' => [
+                    'klient indywidualny' => true,
+                    'Firma' => false
+                ]
+            ])
+            ->add('name', TextType::class, ['label' => 'Imię i nazwisko:'])
+            ->add('street_name', TextType::class, ['label' => 'Ulica, nr domu:'])
+            ->add('street_number', TextType::class, ['label' => false])
+            ->add('city', TextType::class, ['label' => 'Miejscowość, kod pocztowy:'])
+            ->add('postal_code', TextType::class, ['label' => false])
+            ->add('voivodeship', ChoiceType::class, [
+                'label' => 'Województwo:',
+                'expanded' => false,
+                'multiple' => false,
+                'choices' =>
+                [
+                    'Wczytywanie...' => ''
+                ]
+                ])
+            ->add('dial_code', TextType::class, [
+                'label' => false,
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => ' +48'
+                ]
+                ])
+            ->add('phone', TextType::class, ['label' => 'Telefon:'])
+            ->add('email', TextType::class, ['label' => 'Email:'])
+            ->add('pesel', TextType::class, ['label' => 'PESEL:'])
+            ->add('nip', TextType::class, [
+                'label' => 'NIP:',
+                'required' => false
+                ])
+            ->add('cancel', ButtonType::class, ['label' => 'ANULUJ'])
+            ->add('save', SubmitType::class, ['label' => 'ZAPISZ'])
         ;
     }
 
