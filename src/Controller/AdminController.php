8fba6840f5ba78451b77b9d0e51410596b77277f
Blade Studios <maciejwarakomski@gmail.com>
2022-10-21 15:05:32 +0200

    added basic admin view (modifying and deleting not implemented at the moment)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,18 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Client;
+use Doctrine\Persistence\ManagerRegistry;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="app_admin")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $clients = $doctrine->getRepository(Client::class)->findAll();
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'clients' => $clients
         ]);
     }
 }
