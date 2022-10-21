90f5a102901fdc55fe4bb0b3b5d7d3c57375909e
Blade Studios <maciejwarakomski@gmail.com>
2022-10-21 12:12:51 +0200

    implemented a method to retrieve voivodeships data from nazwa.pl API

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -5,16 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AjaxController extends AbstractController
 {
     /**
      * @Route("/ajax/voivodeships", name="ajax_voivodeships")
      */
-    public function index(): Response
+    public function index(Request $request)
     {
-        return $this->render('ajax/index.html.twig', [
-            'controller_name' => 'AjaxController',
-        ]);
+        $url = 'http://api.dro.nazwa.pl/';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        $result=curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+
+        if ($info['http_code'] === 200) {
+            $response = new JsonResponse();
+            $response->setContent($result);
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
+
+        return new JsonResponse([]);
     }
 }
